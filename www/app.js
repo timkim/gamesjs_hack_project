aded87d6a6b7ee931c61066ffbd3b63a1af78800
Tim Kim <timk@adobe.com>
2012-03-29 19:05:54 -0700

    set up basic render engine

---
+++ www/app.js
@@ -0,0 +1,20 @@
+window.requestAnimFrame = (function(){
+      return  window.requestAnimationFrame       || 
+              window.webkitRequestAnimationFrame || 
+              window.mozRequestAnimationFrame    || 
+              window.oRequestAnimationFrame      || 
+              window.msRequestAnimationFrame     || 
+              function( callback ){
+                window.setTimeout(callback, 1000 / 60);
+              };
+})();
+
+function render(){
+	
+}
+
+(function animloop(){
+      requestAnimFrame(animloop);
+      render();
+    })();
+    
