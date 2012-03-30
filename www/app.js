ef38a0b2445aed0b82dd2e72814e9f69abfc7c07
Tim Kim <timk@adobe.com>
2012-03-29 19:43:56 -0700

    Added basic image loader

--- www/app.js
+++ www/app.js
@@ -1,3 +1,23 @@
+var theCanvas;
+var ctx;
+var spriteSheet;
+var allImageData = [];
+
+function onBodyLoad(){
+     // draw images 
+     theCanvas = document.getElementById('theCanvas');
+     ctx = theCanvas.getContext('2d');
+     theCanvas.width = document.images.length * document.images[0].width;
+     theCanvas.height = 3 * document.images[0].height;
+     for(i=0;i<document.images.length;i++){
+        ctx.drawImage(document.images[i],i*document.images[0].width,0);
+        
+        // get image data
+        allImageData.push(ctx.getImageData(i*document.images[0].width,0,document.images[0].width, document.images[0].height));
+        }
+}
+
+
 window.requestAnimFrame = (function(){
       return  window.requestAnimationFrame       || 
               window.webkitRequestAnimationFrame || 
